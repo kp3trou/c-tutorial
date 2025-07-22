aa6d603c13f6b18c28c7d2598b5c0803e2694364
kp3trou <kpetrou80@gmail.com>
2025-07-22 22:08:19 +0300

    Adding sample code

--- advnaced_pointers/advanced_pointers.c
+++ advnaced_pointers/advanced_pointers.c
@@ -2,7 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct mystruct{
+	int year;
+	double weight;
+};
+
+struct secstr{
+	int year;
+	double weight;
+} sec_str; // here can use the sec_str directly.is a variable.
+
+struct node {
+	int value;
+	struct node* next;// next has struct node* as data type.
+};
+
+// function declaration
 char* concat(char* s1,char* s2);
+void printhello();
 
 int main(){
 	// Section 1
@@ -42,13 +59,42 @@ int main(){
 	printf("%s\n\n",combined);
 	free(combined);
 
+	struct mystruct my_str;
+	my_str.year = 1980;
+	sec_str.year = 1984;
+
+	struct node* n1;
+	printf("%p\n",n1); // nil
+	//n1->value = 10;// segmentation fault, not initialize the pointer!
+	//printf("%d,\n",n1->value);
+	n1 = malloc(sizeof(struct node));
+	printf("%p\n",n1);// returms the address of the first byte of the block of memory in heap
+	n1->value = 10;// use -> when you have pointer to a struct
+	printf("%d\n\n\n",n1->value);
+	free(n1);
+
 	// Section 3
 	// Dynamic allocated arrays
+	int* ar = malloc(4 * sizeof(int));// Allocates 16 bytes in heap memory.Addr of 1st byte.	
+	ar[0] = 1;
+	ar[1] = 2;
+	ar[2] = 3;
+	ar[3] = 4;
+	for(int i = 0; i < 4; i++)
+		printf("ar[%d] : %d\n",i,ar[i]);
+	free(ar);
 
-
-
-
-
+	// Section 4
+	// function pointers
+	// example 
+	// declaration of function pointer : void(*funcptr)() 
+	// funcptr is a variable pointer
+	// () means the funcptr is function pointer
+	// () no args
+	// void : the function that funcptr points return nothing
+	void(*funcptr)();// declare function pointer
+	funcptr = printhello;// assign the address of the function to the pointer
+	funcptr();
 
 	printf("\n\nThere are a lot more to discover at the previous sections...\n");
 	return 0;
@@ -67,4 +113,8 @@ char* concat(char* s1,char* s2){
 	return result;
 }
 
+// Function print Hello!
+void printhello(){
+	printf("Hello!\n");
+}
 
