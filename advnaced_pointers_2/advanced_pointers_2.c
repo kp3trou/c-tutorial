147a6060636669685c19d80a7d5918eae99c6767
kp3trou <kpetrou80@gmail.com>
2025-07-26 22:04:16 +0300

    Adding more examples

--- advnaced_pointers_2/advanced_pointers_2.c
+++ advnaced_pointers_2/advanced_pointers_2.c
@@ -2,8 +2,11 @@
 
 #define NULL ((void *)0)// null pointer, casts 0 to void * pointer
 
+int *globalpi;// global pointer
+
 // function declaration
 int foobar();
+void foo();
 
 int main(){
 	// Section 1
@@ -90,8 +93,58 @@ int main(){
 
 	// Section 6
 	// Global and static pointers
-	// to be continued ...
+	// if a pointer declared as global or static, it is initialized to NULL
+	// static && global variables are allocated in different memory location from stack, heap,
+	// called data segment.
+	// globalpi and staticpi.
+	// pointer operators :
+	// *  			declare pointer/dereference a pointer
+	// ->  			used to access fields of a structure referenced by a pointer
+	// +    		increment a pointer
+	// -    		decrement a pointer
+	// ==!= 		compares 2 pointers
+	// (data type)  cast, change the type of pointer
 	
+
+	// Section 7
+	// Pointers and arrays,short,char
+	int vector[] = {28,41,7};
+	int *piv = vector;
+	printf("%d\n",*piv);
+	printf("%d\n",piv[0]);
+	piv += 1;
+	printf("%d\n",*piv);
+	printf("%d\n",piv[0]);
+	piv += 1;
+	printf("%d\n",*piv);
+	printf("%d\n\n",piv[0]);
+	int *ar = vector;
+	for (int i = 0; i < 3; i++)
+		printf("%d\n",*(ar+i));
+	short s;
+	printf("size of short is %d bytes\n",sizeof(s));
+	short *ps = &s;
+	char c;
+	printf("size of char is %d byte\n",sizeof(c));
+	char *pc = &c;
+	printf("address ps points before : %p\n",(void*)ps);// convert to decimal
+	ps += 1;
+	printf("address ps points after : %p\n",(void*)ps); // convert to decimal and see 2 bytes incr.
+	ps += 1;
+	printf("address ps points after : %p\n",(void*)ps); // convert to decimal and see 2 bytes incr.
+	// At the previous memory addresses, that ps points,inside them are garbage.
+	// Just to see that using pointer arithmetic ,have access at memory continuation.
+	// The ps pointer is increnment by 2 bytes because sizeof short is 2 bytes.
+	// Becareful with pointers to void and addition :
+	int nu = 5;
+	void *pvi = &nu;
+	printf("%p\n",(int*)pvi);// use this (recommended)
+	printf("%p\n",pvi);
+	pvi = pvi+1;// Syntax warning is some compilers, don't use this.
+	
+	// Subtracking 2 pointers
+	// to be continued ...
+
 	printf("\n\nThere are a lot more to discover at the previous sections...\n\n");
 	return 0;
 }
@@ -100,4 +153,9 @@ int main(){
 int foobar(){
 	return 7;
 }
+
+void foo(){
+	static int *staticpi;
+}
+
 
