d5fd58198c626cd0da237b6675e1d813e3794a88
kp3trou <kpetrou80@gmail.com>
2025-08-16 13:49:26 +0300

    Adding main repo

---
+++ pointers_and_data_structures/pointers_and_data_structures.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+	// Section 1
+	// Pointers and Data structures
+	// Data Structures are ways to organise and store data in memory.
+	// Examples: array, Linked List, Queue, Stack,Tree.
+
+	// 1.1: Linked List
+
+
+
+
+	printf("\n\nThere are a lot more to discover at the previous sections...\n");
+	return 0;
+}
+
+/***  functions definition  ***/
+
